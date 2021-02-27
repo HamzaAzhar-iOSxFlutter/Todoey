977f560f0ddb82c2c69393fefae1c4d95132a9dd
hamzaazhar <hamzaazharcs@gmail.com>
2021-02-28 04:06:05 +0500

    setting up UserDefaults

---
+++ ToDoList/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  ToDoList
+//
+//  Created by Hamza on 28/02/2021.
+//
+
+import UIKit
+
+struct Cells {
+    static let mainTableViewCell = "ToDoItemCell"
+}
+
+struct UserDetails {
+    static let ToDoListArray = "ToDoListArray"
+}
